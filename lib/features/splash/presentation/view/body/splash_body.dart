ec57e407117cb7a54c419060fc09576094c71a40
yousef <youseffathicontact@gmail.com>
2025-05-14 17:45:16 +0300

    Update dependencies in pubspec.yaml and pubspec.lock; enhance SplashView with SplashBody widget

---
+++ lib/features/splash/presentation/view/body/splash_body.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:my_fruits_hub/core/constants/assets.dart';
+
+class SplashBody extends StatelessWidget {
+  const SplashBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset(Assets.assetsImagesPlant),
+          ],
+        ),
+        SvgPicture.asset(Assets.assetsImagesLogo),
+        SvgPicture.asset(
+          Assets.assetsImagesSplashBottom,
+          fit: BoxFit.fill,
+        ),
+      ],
+    );
+  }
+}
--- lib/features/splash/presentation/view/splash_view.dart
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:my_fruits_hub/features/splash/presentation/view/body/splash_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SafeArea(
+        child: SplashBody(),
+      ),
+    );
   }
 }
